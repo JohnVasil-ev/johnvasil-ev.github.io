c28bc8812149401a111abc11fd3287cc8b294fdd
John Vasiliev <84100214+JohnVasil-ev@users.noreply.github.com>
2023-12-06 11:34:33 +0300

    Release 1.0.0 (#2)

--- client/app/seo/base.vue
+++ client/app/seo/base.vue
@@ -1,8 +1,8 @@
 <template>
   <Meta charset="UTF-8" />
-  <Meta name="viewport"
-    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+  <Meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <Meta http-equiv="x-ua-compatible" content="ie=edge" />
   <Meta name="description" content="My personal portfolio" />
   <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+  <Link rel="sitemap" type="application/xml" href="/sitemap.xml" />
 </template>
